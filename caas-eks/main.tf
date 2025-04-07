434846edf89e327061f9a798dabe8148fa6e6292
ankurp-rafay <ankur@rafay.co>
2025-04-07 09:24:22 +0530

    Create main.tf

---
+++ caas-eks/main.tf
@@ -0,0 +1,143 @@
+locals {
+  rolearn = var.account_id != "" && var.linked_role_arn != "" ? format("arn:aws:iam::%s:role/%s", var.account_id, var.linked_role_arn) : null
+}
+
+resource "rafay_eks_cluster" "eks-cluster" {
+  cluster {
+    kind = "Cluster"
+    metadata {
+      name    = var.cluster_name
+      project = var.project
+      labels  = try(var.cluster_labels, null)
+    }
+    spec {
+      type                   = "eks"
+      blueprint              = var.blueprint_name
+      blueprint_version      = var.blueprint_version
+      cloud_provider         = var.cloud_credentials_name
+      cross_account_role_arn = try(local.rolearn, null)
+      cni_provider           = "aws-cni"
+      dynamic "cni_params" {
+        for_each = var.custom_networking ? [var.custom_networking] : []
+        content {
+          dynamic "custom_cni_crd_spec" {
+            for_each = var.secondary_cidr
+            content {
+              name = custom_cni_crd_spec.value.availability_zone
+              cni_spec {
+                subnet          = custom_cni_crd_spec.value.subnet
+                security_groups = try(custom_cni_crd_spec.value.security_groups, null)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  cluster_config {
+    apiversion = "rafay.io/v1alpha5"
+    kind       = "ClusterConfig"
+    metadata {
+      name    = var.cluster_name
+      region  = var.region
+      version = var.k8s_version
+      tags    = try(var.tags, null)
+    }
+    iam {
+      service_role_arn = try(var.service_role_arn, null)
+      with_oidc        = true
+      dynamic "service_accounts" {
+        for_each = var.service_accounts
+        content {
+          metadata {
+            name = service_accounts.value.name
+            namespace = service_accounts.value.namespace
+          }
+          attach_policy_arns = ["${service_accounts.value.attach_policy_arn}"]
+        }
+      }
+    }
+    identity_mappings {
+      arns {
+        arn = "arn:aws:iam::679196758854:role/hardik-vpc-eks-ng-role"
+        group = ["system:bootstrappers", "system:nodes"]
+        username = "system:node:{{EC2PrivateDNSName}}"
+      }
+    }
+    addons {
+      name    = "aws-ebs-csi-driver"
+      version = "latest"
+    }
+    addons {
+      name    = "aws-efs-csi-driver"
+      version = "latest"
+    }
+    addons {
+      name    = "vpc-cni"
+      version = "latest"
+    }
+    addons {
+      name    = "kube-proxy"
+      version = "latest"
+    }
+    addons {
+      name    = "coredns"
+      version = "latest"
+    }
+    vpc {
+      cluster_endpoints {
+        private_access = var.private_access
+        public_access  = var.public_access
+      }
+      nat {
+        gateway = "Single"
+      }
+      cidr = var.create_vpc ? var.vpc_cidr : null
+
+      dynamic "subnets" {
+        for_each = var.create_vpc ? [] : [var.create_vpc]
+        content {
+          dynamic "private" {
+            for_each = length(var.private_subnets) > 0 ? toset(var.private_subnets) : []
+            content {
+              name = private.value
+              id   = private.value
+            }
+          }
+          dynamic "public" {
+            for_each = length(var.public_subnets) > 0 ? toset(var.public_subnets) : []
+            content {
+              name = public.value
+              id   = public.value
+            }
+          }
+        }
+      }
+    }
+    dynamic "managed_nodegroups" {
+      for_each = var.managed_nodegroups
+      content {
+        name               = managed_nodegroups.value.ami == "" ? "amazonlinux-ami-${managed_nodegroups.key}" : "custom-ami-${managed_nodegroups.key}"
+        instance_type      = managed_nodegroups.value.instance_type
+        desired_capacity   = managed_nodegroups.value.node_count
+        min_size           = managed_nodegroups.value.min_size
+        max_size           = managed_nodegroups.value.max_size
+        volume_size        = 80
+        volume_type        = "gp3"
+        version            = managed_nodegroups.value.ami == "" ? var.k8s_version : null
+        ami                = managed_nodegroups.value.ami != "" ? managed_nodegroups.value.ami : null
+        ami_family         = managed_nodegroups.value.ami_family
+        private_networking = managed_nodegroups.value.private_networking
+        volume_encrypted   = managed_nodegroups.value.volume_encrypted
+        dynamic "iam" {
+          for_each = managed_nodegroups.value.instance_role_arn == null ? [] : [managed_nodegroups.value.instance_role_arn]
+          content {
+            instance_role_arn = iam.value
+          }
+        }
+        tags   = try(managed_nodegroups.value.tags, null)
+        labels = try(managed_nodegroups.value.labels, null)
+      }
+    }
+  }
+}
